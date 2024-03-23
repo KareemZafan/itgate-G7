d5b456631d1807413a1ba278082ae2320e15ac41
KareemZafan <kareemm.zafan@gmail.com>
2024-03-23 21:14:37 +0200

    Implementing Login Funcionalities

---
+++ src/login.py
@@ -0,0 +1,14 @@
+# Code for FB login 
+print("I can login using my Facebook account")
+
+# Code for Google login 
+print("I can login using my Facebook account")
+
+# Code for Username & Password login 
+print("I can login using my username and password account")
+
+# Code for GitHub login 
+print("I can login using my GitHub account")
+
+
+
